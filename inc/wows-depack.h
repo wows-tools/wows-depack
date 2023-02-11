74d84c046335ee3b55388c83cd0488b7a33459e3
kakwa <carpentier.pf@gmail.com>
2023-02-11 16:42:16 +0100

    add header file for a library + struct describing the format

---
+++ inc/wows-depack.h
@@ -0,0 +1,56 @@
+#include <stdint.h>
+
+// INDEX file header
+typedef struct {
+	uint32_t magic;
+	uint32_t unknown_1;
+	uint32_t id;
+	uint32_t unknown_2;
+	uint32_t file_plus_dir_count;
+	uint32_t file_count;
+	uint64_t unknown_3;
+	uint64_t header_size;
+	uint64_t offset_idx_data_section;
+	uint64_t offset_idx_footer_section;
+} WOWS_INDEX_HEADER;
+
+// INDEX file metadata entry
+typedef struct {
+	uint64_t file_type_1;
+	uint64_t offset_idx_file_name;
+	uint64_t unknown_4;
+	uint64_t file_type_2;
+} WOWS_INDEX_METADATA_ENTRY;
+
+// INDEX file pkg data pointer entry
+typedef struct {
+	uint64_t unknown_5;
+	uint64_t unknown_6;
+	uint64_t offset_pkg_data_chunk;
+	uint32_t type_1;
+	uint32_t type_2;
+	uint32_t size_pkg_data_chunk;
+	uint64_t id_pkg_data_chunk;
+	uint32_t padding;
+} WOWS_INDEX_DATA_FILE_ENTRY;
+
+// INDEX file footer
+typedef struct {
+	uint64_t size_pkg_file_name;
+	uint64_t unknown_7;
+	uint64_t unknown_6;
+} WOWS_INDEX_FOOTER;
+
+// PKG file ID + padding
+typedef struct {
+	uint32_t padding_1;
+	uint64_t id_pkg_data_chunk;
+	uint32_t padding_2;
+} WOWS_PKG_ID_ENTRY;
+
+
+int wows_inflate(FILE *source, FILE *dest, long *read);
+int wows_inflate(FILE *source, FILE *dest, long *read);
+void wows_zerr(int ret);
+int wows_is_dir(const char *path);
+int wows_inflate_all(FILE *in, char *outdir);
