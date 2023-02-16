8de2001f26befefd9964cae7dd91cbc3f5eed0ce
kakwa <carpentier.pf@gmail.com>
2023-02-16 11:49:25 +0100

    format

--- lib/index-parser.c
+++ lib/index-parser.c
@@ -2,50 +2,50 @@
 #include <stdio.h>
 #include "wows-depack.h"
 
-
 int print_header(WOWS_INDEX_HEADER *header) {
-	printf("Index Header Content:\n");
-	printf("* magic:                     %.4s\n", (char *)&header->magic);
-	printf("* unknown_1:                 0x%x\n", header->unknown_1);
-	printf("* id:                        0x%x\n", header->id);
-	printf("* unknown_2:                 0x%x\n", header->unknown_2);
-	printf("* file_plus_dir_count:       %u\n", header->file_plus_dir_count);
-	printf("* file_count:                %u\n", header->file_count);
-	printf("* unknown_3:                 %lu\n", header->unknown_3);
-	printf("* header_size:               %lu\n", header->header_size);
-	printf("* offset_idx_data_section:   0x%lx\n", header->offset_idx_data_section);
-	printf("* offset_idx_footer_section: 0x%lx\n", header->offset_idx_footer_section);
-	return 0;
+    printf("Index Header Content:\n");
+    printf("* magic:                     %.4s\n", (char *)&header->magic);
+    printf("* unknown_1:                 0x%x\n", header->unknown_1);
+    printf("* id:                        0x%x\n", header->id);
+    printf("* unknown_2:                 0x%x\n", header->unknown_2);
+    printf("* file_plus_dir_count:       %u\n", header->file_plus_dir_count);
+    printf("* file_count:                %u\n", header->file_count);
+    printf("* unknown_3:                 %lu\n", header->unknown_3);
+    printf("* header_size:               %lu\n", header->header_size);
+    printf("* offset_idx_data_section:   0x%lx\n",
+           header->offset_idx_data_section);
+    printf("* offset_idx_footer_section: 0x%lx\n",
+           header->offset_idx_footer_section);
+    return 0;
 }
 
-int print_metadata_entry(WOWS_INDEX_METADATA_ENTRY *entry, int index){
-	printf("Metadata entry %d:\n", index);
-	printf("* file_type:                 %lu\n", entry->file_type_1);
-	printf("* offset_idx_file_name:      %lx\n", entry->offset_idx_file_name);
-	printf("* unknown_4:                 %lx\n", entry->unknown_4);
-	printf("* file_type_2:               %lu\n", entry->file_type_2);
-	return 0;
-	
+int print_metadata_entry(WOWS_INDEX_METADATA_ENTRY *entry, int index) {
+    printf("Metadata entry %d:\n", index);
+    printf("* file_type:                 %lu\n", entry->file_type_1);
+    printf("* offset_idx_file_name:      %lx\n", entry->offset_idx_file_name);
+    printf("* unknown_4:                 %lx\n", entry->unknown_4);
+    printf("* file_type_2:               %lu\n", entry->file_type_2);
+    return 0;
 }
 
 int wows_parse_index(char *contents, size_t length, WOWS_CONTEXT *context) {
-	// TODO overflow, check size
+    // TODO overflow, check size
 
-	// Get the header section
-	WOWS_INDEX_HEADER *header = (WOWS_INDEX_HEADER *)contents;
-	if (context->debug) {
-		print_header(header);
-	}
+    // Get the header section
+    WOWS_INDEX_HEADER *header = (WOWS_INDEX_HEADER *)contents;
+    if (context->debug) {
+        print_header(header);
+    }
 
+    // TODO overflow, check size
 
-	// TODO overflow, check size
+    // Recover the start of the metadata array
+    WOWS_INDEX_METADATA_ENTRY *metadatas;
+    metadatas =
+        (WOWS_INDEX_METADATA_ENTRY *)contents + sizeof(WOWS_INDEX_HEADER);
 
-	// Recover the start of the metadata array
-	WOWS_INDEX_METADATA_ENTRY *metadatas;
-	metadatas = (WOWS_INDEX_METADATA_ENTRY *)contents + sizeof(WOWS_INDEX_HEADER);
-
-	if (context->debug) {
-		print_metadata_entry(metadatas, 0);
-	}
-	return 0;
+    if (context->debug) {
+        print_metadata_entry(metadatas, 0);
+    }
+    return 0;
 }
--- lib/utils.c
+++ lib/utils.c
@@ -26,177 +26,175 @@
    the version of the library linked do not match, or Z_ERRNO if there
    is an error reading or writing the files. */
 int wows_inflate(FILE *source, FILE *dest, long *read) {
-  int ret;
-  unsigned have;
-  z_stream strm;
-  unsigned char in[CHUNK];
-  unsigned char out[CHUNK];
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
 
-  /* allocate inflate state */
-  strm.zalloc = Z_NULL;
-  strm.zfree = Z_NULL;
-  strm.opaque = Z_NULL;
-  strm.avail_in = 0;
-  strm.next_in = Z_NULL;
-  // ret = inflateInit(&strm);
-  ret = inflateInit2(&strm, -15);
-  if (ret != Z_OK)
-    return ret;
-
-  /* decompress until deflate stream ends or end of file */
-  do {
-    strm.avail_in = fread(in, 1, CHUNK, source);
-    if (ferror(source)) {
-      (void)inflateEnd(&strm);
-      return Z_ERRNO;
-    }
-    if (strm.avail_in == 0)
-      break;
-    strm.next_in = in;
-
-    /* run inflate() on input until output buffer not full */
-    do {
-      strm.avail_out = CHUNK;
-      strm.next_out = out;
-      ret = inflate(&strm, Z_NO_FLUSH);
-      // ret = inflateSync(&strm);
-      assert(ret != Z_STREAM_ERROR); /* state not clobbered */
-      switch (ret) {
-      case Z_NEED_DICT:
-        ret = Z_DATA_ERROR; /* and fall through */
-      case Z_DATA_ERROR:
-      case Z_MEM_ERROR:
-        (void)inflateEnd(&strm);
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+    // ret = inflateInit(&strm);
+    ret = inflateInit2(&strm, -15);
+    if (ret != Z_OK)
         return ret;
-      }
-      have = CHUNK - strm.avail_out;
-      if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-        (void)inflateEnd(&strm);
-        return Z_ERRNO;
-      }
-    } while (strm.avail_out == 0);
 
-    /* done when inflate() says it's done */
-  } while (ret != Z_STREAM_END);
+    /* decompress until deflate stream ends or end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)inflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        if (strm.avail_in == 0)
+            break;
+        strm.next_in = in;
 
-  if (ret == Z_STREAM_END) {
-    *read = strm.total_in;
-  }
+        /* run inflate() on input until output buffer not full */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = inflate(&strm, Z_NO_FLUSH);
+            // ret = inflateSync(&strm);
+            assert(ret != Z_STREAM_ERROR); /* state not clobbered */
+            switch (ret) {
+            case Z_NEED_DICT:
+                ret = Z_DATA_ERROR; /* and fall through */
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                (void)inflateEnd(&strm);
+                return ret;
+            }
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)inflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
 
-  /* clean up and return */
-  (void)inflateEnd(&strm);
-  return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+        /* done when inflate() says it's done */
+    } while (ret != Z_STREAM_END);
+
+    if (ret == Z_STREAM_END) {
+        *read = strm.total_in;
+    }
+
+    /* clean up and return */
+    (void)inflateEnd(&strm);
+    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
 }
 
 /* report a zlib or i/o error */
 void wows_zerr(int ret) {
-  fputs("zpipe: ", stderr);
-  switch (ret) {
-  case Z_ERRNO:
-    if (ferror(stdin))
-      fputs("error reading stdin\n", stderr);
-    if (ferror(stdout))
-      fputs("error writing stdout\n", stderr);
-    break;
-  case Z_STREAM_ERROR:
-    fputs("invalid compression level\n", stderr);
-    break;
-  case Z_DATA_ERROR:
-    fputs("invalid or incomplete deflate data\n", stderr);
-    break;
-  case Z_MEM_ERROR:
-    fputs("out of memory\n", stderr);
-    break;
-  case Z_VERSION_ERROR:
-    fputs("zlib version mismatch!\n", stderr);
-  }
+    fputs("zpipe: ", stderr);
+    switch (ret) {
+    case Z_ERRNO:
+        if (ferror(stdin))
+            fputs("error reading stdin\n", stderr);
+        if (ferror(stdout))
+            fputs("error writing stdout\n", stderr);
+        break;
+    case Z_STREAM_ERROR:
+        fputs("invalid compression level\n", stderr);
+        break;
+    case Z_DATA_ERROR:
+        fputs("invalid or incomplete deflate data\n", stderr);
+        break;
+    case Z_MEM_ERROR:
+        fputs("out of memory\n", stderr);
+        break;
+    case Z_VERSION_ERROR:
+        fputs("zlib version mismatch!\n", stderr);
+    }
 }
 
 int wows_is_dir(const char *path) {
-  struct stat statbuf;
-  if (stat(path, &statbuf) != 0)
-    return 0;
-  return S_ISDIR(statbuf.st_mode);
-}   
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return S_ISDIR(statbuf.st_mode);
+}
 
 int temp_file(char *dir, char **out) {
-  FILE *stream;
-  size_t len;
-  stream = open_memstream(out, &len);
-  fprintf(stream, "%s/bf-XXXXXXX", dir);
-  fflush(stream);
-  fclose(stream);
-  return mkstemp(*out);
+    FILE *stream;
+    size_t len;
+    stream = open_memstream(out, &len);
+    fprintf(stream, "%s/bf-XXXXXXX", dir);
+    fflush(stream);
+    fclose(stream);
+    return mkstemp(*out);
 }
 
 int filename(char *dir, long start, long end, char **out) {
-  FILE *stream;
-  size_t len;
-  stream = open_memstream(out, &len);
-  fprintf(stream, "%s/%010lX-%010lX", dir, start, end);
-  fflush(stream);
-  fclose(stream);
-  return 0;
+    FILE *stream;
+    size_t len;
+    stream = open_memstream(out, &len);
+    fprintf(stream, "%s/%010lX-%010lX", dir, start, end);
+    fflush(stream);
+    fclose(stream);
+    return 0;
 }
 
 int wows_inflate_all(FILE *in, char *outdir) {
-  // number of in bytes read in the last
-  // successful inflate call
-  long read = 0;
-  // return code for inf() (inflate)
-  int ret_inf = 99;
-  // return code for seek()
-  int ret_seek = 0;
-  // offset in the input file
-  long offset = 0;
-  // previous offset
-  long old_offset = 0;
+    // number of in bytes read in the last
+    // successful inflate call
+    long read = 0;
+    // return code for inf() (inflate)
+    int ret_inf = 99;
+    // return code for seek()
+    int ret_seek = 0;
+    // offset in the input file
+    long offset = 0;
+    // previous offset
+    long old_offset = 0;
 
-  // Recover the size of the input
-  int fd = fileno(in);
-  struct stat buf;
-  fstat(fd, &buf);
-  off_t size = buf.st_size;
+    // Recover the size of the input
+    int fd = fileno(in);
+    struct stat buf;
+    fstat(fd, &buf);
+    off_t size = buf.st_size;
 
-  char *tmp_file_name;
-  char *file_name;
-  int tmp_file_fd;
-  FILE *out = NULL;
-  tmp_file_fd = temp_file(outdir, &tmp_file_name);
-  out = fdopen(tmp_file_fd, "w+");
+    char *tmp_file_name;
+    char *file_name;
+    int tmp_file_fd;
+    FILE *out = NULL;
+    tmp_file_fd = temp_file(outdir, &tmp_file_name);
+    out = fdopen(tmp_file_fd, "w+");
 
-  while (ret_inf != Z_OK || ret_seek == 0) {
-
-    ret_seek = fseek(in, offset, SEEK_SET);
-    offset += 1;
-    ret_inf = wows_inflate(in, out, &read);
-    // if (ret != Z_OK)
-    //    zerr(ret);
-    if (ret_inf == Z_OK) {
-      fclose(out);
-      fprintf(stderr, "return seek  %d\n", ret_seek);
-      fprintf(stderr, "cur position %ld\n", ftell(in));
-      fprintf(stderr, "read  %ld\n", read);
-      old_offset = offset;
-      offset += read;
-      fprintf(stderr, "offset  %lX -> %lX\n", old_offset, offset);
-      filename(outdir, old_offset, offset, &file_name);
-      rename(tmp_file_name, file_name);
-      free(file_name);
-      free(tmp_file_name);
-      if (offset < size) {
-        tmp_file_fd = temp_file(outdir, &tmp_file_name);
-        out = fdopen(tmp_file_fd, "w+");
-      } else {
-        break;
-      }
+    while (ret_inf != Z_OK || ret_seek == 0) {
+        ret_seek = fseek(in, offset, SEEK_SET);
+        offset += 1;
+        ret_inf = wows_inflate(in, out, &read);
+        // if (ret != Z_OK)
+        //    zerr(ret);
+        if (ret_inf == Z_OK) {
+            fclose(out);
+            fprintf(stderr, "return seek  %d\n", ret_seek);
+            fprintf(stderr, "cur position %ld\n", ftell(in));
+            fprintf(stderr, "read  %ld\n", read);
+            old_offset = offset;
+            offset += read;
+            fprintf(stderr, "offset  %lX -> %lX\n", old_offset, offset);
+            filename(outdir, old_offset, offset, &file_name);
+            rename(tmp_file_name, file_name);
+            free(file_name);
+            free(tmp_file_name);
+            if (offset < size) {
+                tmp_file_fd = temp_file(outdir, &tmp_file_name);
+                out = fdopen(tmp_file_fd, "w+");
+            } else {
+                break;
+            }
+        }
+        // If we are beyond the file
+        // stop
+        if (offset > size) {
+            break;
+        }
     }
-    // If we are beyond the file
-    // stop
-    if (offset > size) {
-      break;
-    }
-  }
-  return ret_inf;
+    return ret_inf;
 }
-
