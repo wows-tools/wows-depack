4a2270f85dd18c99676808e64ca7316b69c2a22a
kakwa <carpentier.pf@gmail.com>
2023-02-17 01:19:32 +0100

    reordering of the code + dump of the pkg file entries

--- lib/index-parser.c
+++ lib/index-parser.c
@@ -28,11 +28,25 @@ int print_footer(WOWS_INDEX_FOOTER *footer) {
 }
 
 int print_metadata_entry(WOWS_INDEX_METADATA_ENTRY *entry, int index) {
-    printf("Metadata entry %d:\n", index);
+    printf("Metadata entry [%d]:\n", index);
     printf("* file_name_size:            %lu\n", entry->file_name_size);
     printf("* offset_idx_file_name:      0x%lx\n", entry->offset_idx_file_name);
     printf("* unknown_4:                 0x%lx\n", entry->unknown_4);
     printf("* file_type_2:               0x%lx\n", entry->file_type_2);
+    return 0;
+}
+
+int print_data_file_entry(WOWS_INDEX_DATA_FILE_ENTRY *entry, int index) {
+    printf("Data file entry [%d]:\n", index);
+    printf("* unknown_5:                 0x%lx\n", entry->unknown_5);
+    printf("* unknown_6:                 0x%lx\n", entry->unknown_6);
+    printf("* offset_pkg_data_chunk:     0x%lx\n",
+           entry->offset_pkg_data_chunk);
+    printf("* type_1:                    0x%x\n", entry->type_1);
+    printf("* type_2:                    0x%x\n", entry->type_2);
+    printf("* size_pkg_data_chunk:       0x%x\n", entry->size_pkg_data_chunk);
+    printf("* id_pkg_data_chunk:         0x%lx\n", entry->id_pkg_data_chunk);
+    printf("* padding:                   0x%x\n", entry->padding);
     return 0;
 }
 
@@ -42,10 +56,17 @@ int wows_parse_index(char *contents, size_t length, WOWS_CONTEXT *context) {
 
     // Get the header section
     WOWS_INDEX_HEADER *header = (WOWS_INDEX_HEADER *)contents;
-    if (context->debug) {
-        print_header(header);
-        printf("\n");
-    }
+
+    // Get the start of the metadata array
+    WOWS_INDEX_METADATA_ENTRY *metadatas;
+    metadatas =
+        (WOWS_INDEX_METADATA_ENTRY *)(contents + sizeof(WOWS_INDEX_HEADER));
+
+    // Get the start  pkg data pointer section
+    WOWS_INDEX_DATA_FILE_ENTRY *data_file_entry =
+        (WOWS_INDEX_DATA_FILE_ENTRY *)(contents +
+                                       header->offset_idx_data_section +
+                                       MAGIC_SECTION_OFFSET);
 
     // Get the footer section
     WOWS_INDEX_FOOTER *footer =
@@ -53,24 +74,14 @@ int wows_parse_index(char *contents, size_t length, WOWS_CONTEXT *context) {
                               MAGIC_SECTION_OFFSET);
     char *pkg_filename = (char *)footer;
     pkg_filename += sizeof(WOWS_INDEX_FOOTER);
+
+    // Print the header
     if (context->debug) {
-        print_footer(footer);
-        printf("* pkg filename:              %.*s\n",
-               (int)footer->pkg_file_name_size, pkg_filename);
+        print_header(header);
         printf("\n");
     }
 
-    // Get pkg data pointer section
-    WOWS_INDEX_DATA_FILE_ENTRY *data_file_entry =
-        (WOWS_INDEX_DATA_FILE_ENTRY *)(contents +
-                                       header->offset_idx_data_section +
-                                       MAGIC_SECTION_OFFSET);
-
-    // Recover the start of the metadata array
-    WOWS_INDEX_METADATA_ENTRY *metadatas;
-    metadatas =
-        (WOWS_INDEX_METADATA_ENTRY *)(contents + sizeof(WOWS_INDEX_HEADER));
-
+    // Print the metadata entries
     for (i = 0; i < header->file_plus_dir_count; i++) {
         WOWS_INDEX_METADATA_ENTRY *entry = &metadatas[i];
         char *filename = (char *)entry;
@@ -81,6 +92,23 @@ int wows_parse_index(char *contents, size_t length, WOWS_CONTEXT *context) {
                    (int)entry->file_name_size, filename);
             printf("\n");
         }
+    }
+
+    // Print the pkg file entries
+    for (i = 0; i < header->file_count; i++) {
+        WOWS_INDEX_DATA_FILE_ENTRY *entry = &data_file_entry[i];
+        if (context->debug) {
+            print_data_file_entry(entry, i);
+            printf("\n");
+        }
+    }
+
+    // Print the footer
+    if (context->debug) {
+        print_footer(footer);
+        printf("* pkg filename:              %.*s\n",
+               (int)footer->pkg_file_name_size, pkg_filename);
+        printf("\n");
     }
 
     return 0;
