c39ea2adf4255999d407e14f46b5e5be3c365916
kakwa <carpentier.pf@gmail.com>
2023-02-26 01:33:07 +0100

    few more doxygen fixes

--- misc/setup_doxycss.sh
+++ misc/setup_doxycss.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-if ! [ -d doxygen-awesome-css ]
+if ! [ -d docs/doxygen-awesome-css ]
 then
     curl -L https://github.com/jothepro/doxygen-awesome-css/archive/refs/tags/v2.2.0.tar.gz --output doxycss.tar.gz
     tar -xf doxycss.tar.gz -C docs/
     mv docs/doxygen-awesome-css* docs/doxygen-awesome-css
     rm -f doxycss.tar.gz
 else
-    echo "docs/doxygen-awesome-css already exists"
-    echo "please run 'rm -rf \"docs/doxygen-awesome-css\"' to redeploy it"
+    echo "docs/doxygen-awesome-css already exists."
+    echo "please run:"
+    echo "   rm -rf \"docs/doxygen-awesome-css\""
+    echo "to redeploy it."
 fi
