76f3b5c1f276dc117baa12505db5bd44ae279463
kakwa <carpentier.pf@gmail.com>
2023-02-27 19:20:39 +0100

    make get_path_inode work on any inode type instead of limiting it to files.

--- lib/wows-depack-private.h
+++ lib/wows-depack-private.h
@@ -168,7 +168,7 @@ WOWS_DIR_INODE *init_dir_inode(uint64_t metadata_id, uint32_t current_index_cont
                                WOWS_CONTEXT *context);
 WOWS_FILE_INODE *init_file_inode(uint64_t metadata_id, uint32_t current_index_context, WOWS_DIR_INODE *parent_inode,
                                  WOWS_CONTEXT *context);
-int get_path_inode(WOWS_FILE_INODE *inode, int *depth, char *entries[]);
+int get_path_inode(WOWS_BASE_INODE *inode, int *depth, char *entries[]);
 
 /* dump an index in a file */
 int wows_dump_index_to_file(WOWS_INDEX *index, FILE *f);
